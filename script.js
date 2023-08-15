52dc384c2b34249cd77cf73674b5172e58fabba9
PruBlu <64672137+PruBlu@users.noreply.github.com>
2023-08-16 02:08:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 const dynamicButton = document.getElementById('dynamic-button');
-const map = document.querySelector('.map');
-
-window.addEventListener('resize', positionDynamicButton);
-window.addEventListener('load', positionDynamicButton);
-
-function positionDynamicButton() {
-  const mapRect = map.getBoundingClientRect();
-  const offsetX = 200; // Adjust based on your background
-  const offsetY = 100; // Adjust based on your background
-  
-  const buttonX = mapRect.left + offsetX;
-  const buttonY = mapRect.top + offsetY;
-
-  dynamicButton.style.left = `${buttonX}px`;
-  dynamicButton.style.top = `${buttonY}px`;
-}
 
 dynamicButton.addEventListener('mouseover', () => {
   dynamicButton.innerHTML = 'aaaa';
