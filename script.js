dd21721d1e29d2314ede67cc8c0f2b8119f500e5
PruBlu <64672137+PruBlu@users.noreply.github.com>
2023-08-16 17:13:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-const player = document.querySelector('.player');
-const diceButton = document.querySelector('.dice-button');
+const dynamicButton = document.getElementById('dynamic-button');
+const map = document.querySelector('.map');
+
+const diceButton = document.getElementById('dice');
+const resultElement = document.getElementById('result');
 
 diceButton.addEventListener('click', rollDice);
 
 function rollDice() {
-  const randomNum = Math.floor(Math.random() * 6) + 1;
-  diceButton.textContent = `Dice: ${randomNum}`;
-  movePlayer(randomNum);
+  const randomNum = Math.floor(Math.random() * 6) + 1; // Generates random number between 1 and 6
+  resultElement.textContent = `Result: ${randomNum}`;
 }
 
-function movePlayer(steps) {
-  const currentPosition = parseInt(player.style.left) || 0;
-  const newPosition = currentPosition + (steps * 40); // Adjust as needed
-  player.style.left = `${newPosition}px`;
-
-  // Check if player has reached the destination
-  if (newPosition >= 600) {
-    alert('Congratulations! You reached the destination!');
-    player.style.left = '0';
-    diceButton.textContent = 'Roll Dice';
-  }
-}
+// Rest of your script code for dynamic button positioning
