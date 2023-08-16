f34e3ba7d4a8a7e0c9e5fc8f115b49a2fa1be4ed
PruBlu <64672137+PruBlu@users.noreply.github.com>
2023-08-16 18:17:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const dynamicButton = document.getElementById('dynamic-button');
   const map = document.querySelector('.map');
   
-  // Position the dynamic button when the page loads or the window is resized
-  function positionDynamicButton() {
-    const mapRect = map.getBoundingClientRect();
-    const offsetX = Math.floor(mapRect.width / 7); // 1/7 of the horizontal width
-    const offsetY = Math.floor(mapRect.height / 5); // 1/5 of the vertical height
-    
-    const buttonX = mapRect.left + offsetX;
-    const buttonY = mapRect.top + offsetY;
-    
-    dynamicButton.style.left = `${buttonX}px`;
-    dynamicButton.style.top = `${buttonY}px`;
-  }
-  
   // Set up button positioning
   positionDynamicButton();
   window.addEventListener('resize', positionDynamicButton);
