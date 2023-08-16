a0cea1536867615559b23a21c46ac6d460266234
PruBlu <64672137+PruBlu@users.noreply.github.com>
2023-08-16 22:39:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,65 +13,13 @@ function rollDice() {
 
 // Rest of your script code for dynamic button positioning
 
+const diceLabel = document.querySelector('label[for="dice"]');
+const dice = document.querySelector('.dice');
 
-$('#group').click(function() {
-  $('#group').removeAttr('style');
-  setTimeout(function() {
-    var num = Math.ceil(Math.random() * 6);
-            if(num==1){
-                $('#group').css({
-                    'animation':'scroll 2s linear 0s 2 alternate',
-                    '-webkit-animation':'scroll 2s linear 0s 2 alternate',
-                    '-moz-animation':'scroll 2s linear 0s 2 alternate',
-                    'transform':'rotateY(360deg) rotateX(360deg) rotateZ(360deg)',
-                    '-webkit-transform':'rotateY(360deg) rotateX(360deg) rotateZ(360deg)',
-                    '-moz-transform':'rotateY(360deg) rotateX(360deg) rotateZ(360deg)'
-                })
-            }else if(num==2){
-                $('#group').css({
-                    'animation':'scroll 2s linear 0s 2 alternate',
-                    '-webkit-animation':'scroll 2s linear 0s 2 alternate',
-                    '-moz-animation':'scroll 2s linear 0s 2 alternate',
-                    'transform':'rotateY(270deg) rotateX(360deg) rotateZ(360deg)',
-                    '-webkit-transform':'rotateY(270deg) rotateX(360deg) rotateZ(360deg)',
-                    '-moz-transform':'rotateY(270deg) rotateX(360deg) rotateZ(360deg)'
-                })
-            }else if(num==3){
-                $('#group').css({
-                    'animation':'scroll 2s linear 0s 2 alternate',
-                    '-webkit-animation':'scroll 2s linear 0s 2 alternate',
-                    '-moz-animation':'scroll 2s linear 0s 2 alternate',
-                    'transform':'rotateY(180deg) rotateX(270deg) rotateZ(270deg)',
-                    '-webkit-transform':'rotateY(180deg) rotateX(270deg) rotateZ(270deg)',
-                    '-moz-transform':'rotateY(180deg) rotateX(270deg) rotateZ(270deg)'
-                })
-            }else if(num==4){
-                $('#group').css({
-                    'animation':'scroll 2s linear 0s 2 alternate',
-                    '-webkit-animation':'scroll 2s linear 0s 2 alternate',
-                    '-moz-animation':'scroll 2s linear 0s 2 alternate',
-                    'transform':'rotateY(180deg) rotateX(90deg) rotateZ(360deg)',
-                    '-webkit-transform':'rotateY(180deg) rotateX(90deg) rotateZ(360deg)',
-                    '-moz-transform':'rotateY(180deg) rotateX(90deg) rotateZ(360deg)'
-                })
-            }else if(num==5){
-                $('#group').css({
-                    'animation':'scroll 2s linear 0s 2 alternate',
-                    '-webkit-animation':'scroll 2s linear 0s 2 alternate',
-                    '-moz-animation':'scroll 2s linear 0s 2 alternate',
-                    'transform':'rotateY(180deg) rotateX(270deg) rotateZ(360deg)',
-                    '-webkit-transform':'rotateY(180deg) rotateX(270deg) rotateZ(360deg)',
-                    '-moz-transform':'rotateY(180deg) rotateX(270deg) rotateZ(360deg)'
-                })
-            }else if(num==6){
-                $('#group').css({
-                    'animation':'scroll 2s linear 0s 2 alternate',
-                    '-webkit-animation':'scroll 2s linear 0s 2 alternate',
-                    '-moz-animation':'scroll 2s linear 0s 2 alternate',
-                    'transform':'rotateY(180deg) rotateX(360deg) rotateZ(360deg)',
-                    '-webkit-transform':'rotateY(180deg) rotateX(360deg) rotateZ(360deg)',
-                    '-moz-transform':'rotateY(180deg) rotateX(360deg) rotateZ(360deg)'
-                })
+diceLabel.addEventListener('click', function () {
+  dice.style.animationPlayState = 'paused';
+});
+
             } 
         },1)
     })
